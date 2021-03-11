2754d9190c4cfa59988e7e639bfd2ccc0e9849f5
JackieBinya <jacquelinebinya@gmail.com>
2021-03-11 15:31:52 +0200

    Adds stripe checkout serverless function

---
+++ api/create-checkout-session/index.js
@@ -0,0 +1,25 @@
+const stripe = require('stripe')('sk_test_51IP1D3CD5ZUxyIJEJEj26gjKOBaWPVFTCigOU4g9PNSvMe5ZS8kyc9hdO7J9MO8YgiUPUu0fApzU2l7fspER7Xco00wRapK4fy')
+
+
+module.exports = async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+          {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: 'T-shirt',
+              },
+              unit_amount: 2000,
+            },
+            quantity: 1,
+          },
+        ],
+        mode: 'payment',
+        success_url: 'https://example.com/success',
+        cancel_url: 'https://example.com/cancel',
+      });
+    
+      res.json({ id: session.id });
+  }
