f207737c7e5fd28082d10fafcb0851ac78ed9bbb
JackieBinya <jacquelinebinya@gmail.com>
2021-03-12 07:07:37 +0200

    🧑‍🍳

--- api/customers/index.js
+++ api/customers/index.js
@@ -1,9 +1,7 @@
 const stripe = require('stripe')('sk_test_51IP1D3CD5ZUxyIJEJEj26gjKOBaWPVFTCigOU4g9PNSvMe5ZS8kyc9hdO7J9MO8YgiUPUu0fApzU2l7fspER7Xco00wRapK4fy');
 
 module.exports = async (req, res) => {
-    const { body } = req
-
-    const {name} = body
+    console.log(req.body)
 
     // const customer = await stripe.customers.create({
     //     name: body.name,
