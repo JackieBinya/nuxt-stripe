abe4f29898db1b51ed9392b891c81ed096fef8e0
JackieBinya <jacquelinebinya@gmail.com>
2021-03-12 13:25:08 +0200

    Changes route

--- pages/register/_slug/index.vue
+++ pages/register/_slug/index.vue
@@ -1,49 +1,52 @@
 <template>
-    <form @submit.prevent="createCustomer">
-        <!-- <input v-model="customer.name" class="border py-2 px-3 text-grey-darkest" type="text" placeholder="Name" /> -->
-        <input v-model="customer.email" class="border py-2 px-3 text-grey-darkest" type="text" placeholder="Work Email" />
-        <button v-if="slug === corePriceId" type="submit">Free Trial</button>
-        <button v-else type="submit">Continue</button>
-    </form>
+  <form @submit.prevent="createCustomer">
+    <!-- <input v-model="customer.name" class="border py-2 px-3 text-grey-darkest" type="text" placeholder="Name" /> -->
+    <input
+      v-model="customer.email"
+      class="border py-2 px-3 text-grey-darkest"
+      type="text"
+      placeholder="Work Email"
+    />
+    <button v-if="slug === corePriceId" type="submit">Free Trial</button>
+    <button v-else type="submit">Continue</button>
+  </form>
 </template>
 <script>
-    export default{
-        data(){
-            return{
-                slug: this.$route.params.slug,
-                corePriceId: "price_1IRIwiCD5ZUxyIJEU4jaghEZ",
-               customer: {}
-            }
+export default {
+  data() {
+    return {
+      slug: this.$route.params.slug,
+      corePriceId: "price_1IRIwiCD5ZUxyIJEU4jaghEZ",
+      customer: {},
+    };
+  },
+  methods: {
+    async createCustomer() {
+      const result = await fetch("https://nuxt-stripe-v5.vercel.app/customers/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        methods:{
-        async createCustomer (){
-            
-            const result = await fetch('api/customers', {
-                method: 'POST',
-                headers: {
-                'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(this.customer)
-            })
+        body: JSON.stringify(this.customer),
+      });
 
-            const {id} = result.json()
-            this.createFreeTrial(id)
-            },
+      const { id } = result.json();
+      this.createFreeTrial(id);
+    },
 
-        async createFreeTrial (id){
-         const result  = await fetch('api/customers', {
-                method: 'POST',
-                headers: {
-                'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({id})
-                })
+    async createFreeTrial(id) {
+      const result = await fetch("api/customers", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id }),
+      });
 
-                console.log(result.json())
-           }
-        }
-    }
+      console.log(result.json());
+    },
+  },
+};
 </script>
 <style scoped>
-
 </style>
