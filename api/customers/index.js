52415d7f47cd936aed56a33fc98b0d4e76f21df5
JackieBinya <jacquelinebinya@gmail.com>
2021-03-12 12:47:25 +0200

    Create free trial

--- api/customers/index.js
+++ api/customers/index.js
@@ -3,16 +3,17 @@ const stripe = require('stripe')('sk_test_51IP1D3CD5ZUxyIJEJEj26gjKOBaWPVFTCigOU
 module.exports = async (req, res) => {
     // const example = req.body
 
-    const customer = await stripe.customers.create({
+    const {id} = await stripe.customers.create({
         email: req.body.email,
     });
 
-   // $ curl - d "name=kelly&email=kelly.live.com" - X POST https://nuxt-stripe-v5-d79bth1an-jackiebinya.vercel.app/api/customers
-    //{ "name": "kelly" }
-
-    res.send({ id: customer.id })
-
-    // console.log("Here")
-
-    // res.send({name: req.body.name})
+    await stripe.subscriptions.create({
+        customer: id,
+        items: [
+          {
+            price: 'price_1IRIwiCD5ZUxyIJEU4jaghEZ',
+          },
+        ],
+        trial_period_days: 15,
+      })
 }
